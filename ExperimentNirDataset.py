7edbb2894c5d6e663b4766d794ac25adf5c4196b
Shaked <shaked3462@gmail.com>
2019-06-08 10:34:19 +0000

    a

--- ExperimentNirDataset.py
+++ ExperimentNirDataset.py
@@ -1,15 +1,47 @@
 import numpy as np
 from sklearn.utils import shuffle
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+import logging
+import os.path
+import time
+from collections import OrderedDict
+import sys
+import torch.nn.functional as F
+from torch import optim
+import torch as th
+from braindecode.models.deep4 import Deep4Net
+from braindecode.experiments.experiment import Experiment
+from braindecode.experiments.monitors import LossMonitor, MisclassMonitor, \
+    RuntimeMonitor
+from braindecode.experiments.stopcriteria import MaxEpochs, NoDecrease, Or
+from braindecode.datautil.iterators import BalancedBatchSizeIterator
+from braindecode.models.shallow_fbcsp import ShallowFBCSPNet
+from braindecode.datautil.splitters import split_into_two_sets
+from braindecode.torch_ext.constraints import MaxNormDefaultConstraint
+from braindecode.torch_ext.util import set_random_seeds, np_to_var
+from braindecode.mne_ext.signalproc import mne_apply
+from braindecode.datautil.signalproc import (bandpass_cnt,
+                                             exponential_running_standardize)
+from braindecode.datautil.trial_segment import create_signal_target_from_raw_mne
+from braindecode.datautil.signal_target import SignalAndTarget
 
 
-def loadSubjects(subjectNum):
+def loadSubjects(subjectNum, single_subject, single_subject_num):
     label0Counter = 0
     label1Counter = 0
     label2Counter = 0
 
-    print("loading data for subject 1")
-    X = np.load("NirDataset\\croppedDataForSubject1.npy")
-    y = np.load("NirDataset\\croppedLabelsForSubject1.npy")
+    if single_subject == True:
+        print("loading data for subject {}".format(single_subject_num))
+        X = np.load("NirDataset\\croppedDataForSubject{}.npy".format(single_subject_num))
+        y = np.load("NirDataset\\croppedLabelsForSubject{}.npy".format(single_subject_num))
+    else:        
+        print("loading data for subject 1")
+        X = np.load("NirDataset\\croppedDataForSubject1.npy")
+        y = np.load("NirDataset\\croppedLabelsForSubject1.npy")
 
     for i in range(0,len(y)):
         if y[i] == 0:
@@ -19,14 +51,14 @@ def loadSubjects(subjectNum):
         if y[i] == 2:
             label2Counter += 1
     print("INFO : label 0 examples: {}, label 1 examples: {}, label 2 examples: {}".format(label0Counter, label1Counter, label2Counter))
-    print("INFO : used {} examples from each label".format(label0Counter))
     examplesPerSubjecti = np.amin([label0Counter, label1Counter, label2Counter])
+    print("INFO : used {} examples from each label".format(examplesPerSubjecti))
     print(X.shape)
     X0 = X[:label0Counter,:,:]
-    X1 = X[label0Counter:label1Counter,:,:]
+    X1 = X[label0Counter:label0Counter+label1Counter,:,:]
     X2 = X[label0Counter+label1Counter:,:,:]
     y0 = y[:label0Counter]
-    y1 = y[label0Counter:label1Counter]
+    y1 = y[label0Counter:label0Counter+label1Counter]
     y2 = y[label0Counter+label1Counter:]
 
     if subjectNum > 1:
@@ -52,10 +84,10 @@ def loadSubjects(subjectNum):
             print("INFO : used {} examples from each label".format(examplesPerSubjecti))
     
             Xtmp0 = Xtmp[:label0Counter,:,:]
-            Xtmp1 = Xtmp[label0Counter:label1Counter,:,:]
+            Xtmp1 = Xtmp[label0Counter:label0Counter+label1Counter,:,:]
             Xtmp2 = Xtmp[label0Counter+label1Counter:,:,:]
             ytmp0 = ytmp[:label0Counter]
-            ytmp1 = ytmp[label0Counter:label1Counter]
+            ytmp1 = ytmp[label0Counter:label0Counter+label1Counter]
             ytmp2 = ytmp[label0Counter+label1Counter:]
             X0 = np.concatenate((X0, Xtmp0), axis=0)
             X1 = np.concatenate((X1, Xtmp1), axis=0)
@@ -63,11 +95,9 @@ def loadSubjects(subjectNum):
             y0 = np.concatenate((y0, ytmp0), axis=0)
             y1 = np.concatenate((y1, ytmp1), axis=0)
             y2 = np.concatenate((y2, ytmp2), axis=0)
-            print("X0 shape {}".format(X0.shape))
-            print("X1 shape {}".format(X1.shape))
-            print("X2 shape {}".format(X2.shape))
-
-    print("INFO : SUBJECTS NUM : {} ".format(subjectNum))
+            # print("X0 shape {}".format(X0.shape))
+            # print("X1 shape {}".format(X1.shape))
+            # print("X2 shape {}".format(X2.shape))
 
     X0, y0 = shuffle(X0, y0)
     X1, y1 = shuffle(X1, y1)
@@ -77,14 +107,25 @@ def loadSubjects(subjectNum):
     X = np.concatenate((X0[:examplesPerAllSubjects], X1[:examplesPerAllSubjects], X2[:examplesPerAllSubjects]), axis=0)
     y = np.concatenate((y0[:examplesPerAllSubjects], y1[:examplesPerAllSubjects], y2[:examplesPerAllSubjects]), axis=0)
 
-    print("trying to run with 2.5 sec trials")
-
     return X, y
 
-X, y = loadSubjects(44)
-batch_size = 8
-epoches = 100
+def plotConfusionMatrix(confusion_matrix):
+    print(confusion_matrix)
+    df_cm = pd.DataFrame(confusion_matrix, range(3), range(3))
+    #plt.figure(figsize = (10,7))
+    sn.set(font_scale=1.4)#for label size
+    sn.heatmap(df_cm, annot=True, cmap='Blues', annot_kws={"size": 16}, fmt='d')# font size
+    plt.show()
+
 model_type = 'shallow'
+single_subject_num = sys.argv[1]
+
+
+single_subject = True
+X, y = loadSubjects(52, single_subject, single_subject_num)
+batch_size = 16
+epoches = 800
+cuda = True
 
 # Enable logging
 import logging
@@ -104,8 +145,8 @@ X, y = shuffle(X, y)
 print("y")
 print(y)
 from braindecode.datautil.signal_target import SignalAndTarget
-trainingSampleSize = int(len(X)*0.7)
-valudationSampleSize = int(len(X)*0.1)
+trainingSampleSize = int(len(X)*0.65)
+valudationSampleSize = int(len(X)*0.15)
 testSampleSize = int(len(X)*0.2)
 print("INFO : Training sample size: {}".format(trainingSampleSize))
 print("INFO : Validation sample size: {}".format(valudationSampleSize))
@@ -123,7 +164,6 @@ from braindecode.torch_ext.util import set_random_seeds
 
 # Set if you want to use GPU
 # You can also use torch.cuda.is_available() to determine if cuda is available on your machine.
-cuda = False
 set_random_seeds(seed=20170629, cuda=cuda)
 n_classes = 3
 in_chans = train_set.X.shape[1]
@@ -131,25 +171,26 @@ print("INFO : in_chans: {}".format(in_chans))
 print("INFO : input_time_length: {}".format(train_set.X.shape[2]))
 
 # final_conv_length = auto ensures we only get a single output in the time dimension
-if model_type == 'shallow':
-    model = ShallowFBCSPNet(in_chans=in_chans, n_classes=n_classes,
-                        input_time_length=train_set.X.shape[2],
-                        final_conv_length='auto')
-else:
-    model = Deep4Net(in_chans=in_chans, n_classes=n_classes,
-                        input_time_length=train_set.X.shape[2],
-                        final_conv_length='auto')
-
+model = th.load("models\{}-trialwise-52subjects-2.5sec-800epoches-torch_model".format(model_type))
 if cuda:
     model.cuda()
 
 
 from braindecode.torch_ext.optimizers import AdamW
 import torch.nn.functional as F
-if model_type == 'shallow':
-    optimizer = AdamW(model.parameters(), lr=0.0625 * 0.01, weight_decay=0)
-else:
-    optimizer = AdamW(model.parameters(), lr=1*0.01, weight_decay=0.5*0.001) # these are good values for the deep model
+# if model_type == 'shallow':
+optimizer = AdamW(model.parameters(), lr=0.001, weight_decay=0.01)
+# else:
+#     optimizer = AdamW(model.parameters(), lr=1*0.01, weight_decay=0.01) # these are good values for the deep model
+
+device = th.device("cuda:0" if th.cuda.is_available() else "cpu")
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
+
+model = model.to(device)
+
+
+
 
 model.compile(loss=F.nll_loss, optimizer=optimizer, iterator_seed=1,)
 
@@ -157,48 +198,37 @@ print("INFO : Epochs: {}".format(epoches))
 print("INFO : Batch Size: {}".format(batch_size))
 
 # Run the training
-model.fit(train_set.X, train_set.y, epochs=epoches, batch_size=batch_size, scheduler='cosine',
-         validation_data=(valid_set.X, valid_set.y),)
+print(model.fit(train_set.X, train_set.y, epochs=epoches, batch_size=batch_size, scheduler='cosine',
+         validation_data=(valid_set.X, valid_set.y),))
 
 print(model.epochs_df)
-
+np.save("finetuning-{}-trialwise-52subjects-2.5sec-{}epoches".format(model_type, max_epochs), model.epochs_df.iloc[:])
 # Evaluation
 test_set = SignalAndTarget(X[(trainingSampleSize + valudationSampleSize):], y=y[(trainingSampleSize + valudationSampleSize):])
 
 eval = model.evaluate(test_set.X, test_set.y)
 print(eval)
 
-from sklearn.metrics import confusion_matrix
+plt.plot(model.epochs_df.iloc[:,3], 'g-', label='Train misclass')
+plt.plot(model.epochs_df.iloc[:,4], 'b-', label='Validation misclass')
+plt.title('misclass rate / epoches')
+plt.xlabel('Epoches')
+plt.ylabel('Misclass')
+plt.legend(loc='best')
+plt.show()
+
+# plt.savefig("single_subjects\finetune-{}-trialwise-subject{}-{}sec.png".format(model, single_subject_num, trial_length), bbox_inches='tight')
+
+
 
 try:
     print("prediction")
-    y_pred = model.predict(test_set.X)
+    y_pred = model.predict_classes(test_set.X)
     print(y_pred)
     print("real labels")
     print(test_set.y)
     confusion_matrix = confusion_matrix(test_set.y, y_pred)
-    print(confusion_matrix)
+    plotConfusionMatrix(confusion_matrix)
 except:
-    try:
-        y_pred = model.predict_classes(test_set.X)
-        print(y_pred)
-        print("real labels")
-        print(test_set.y)
-        confusion_matrix = confusion_matrix(test_set.y, y_pred)
-        print(confusion_matrix)
-    except:
-        print("predict_classes method failed")
+    print("predict_classes method failed")
 
-
-import seaborn as sn
-import pandas as pd
-import matplotlib.pyplot as plt
-
-array = confusion_matrix       
-df_cm = pd.DataFrame(array, range(3),
-                  range(3))
-#plt.figure(figsize = (10,7))
-sn.set(font_scale=1.4)#for label size
-sn.heatmap(df_cm, annot=True, cmap='Blues', annot_kws={"size": 16}, fmt='d')# font size
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-plt.show()
