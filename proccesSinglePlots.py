fd690d9e0031ab809ecebd9ea7e9ddb26e7a0d25
Shaked <shaked3462@gmail.com>
2019-06-10 20:59:40 +0000

    a

--- proccesSinglePlots.py
+++ proccesSinglePlots.py
@@ -10,7 +10,7 @@ np.set_printoptions(precision=4, threshold=np.inf)
 
 plotTypes = ['accuracy', 'misclass']
 models = ['deep', 'shallow']
-withTest = True
+withTest = False
 
 for p in plotTypes:
     for j in models:
@@ -21,44 +21,46 @@ for p in plotTypes:
         meanTrain = np.zeros((2, 2000))
         meanVal = np.zeros((2, 2000))
         meanTest = np.zeros((2, 2000))
-        for subject in range(1,53):
+        for subject in range(1,48):
+            if subject == 44 or subject == 45:
+                continue
             try:
-                subjectData = np.load("single_subjects\{}-trialwise-singleSubjectNum{}-2.5sec-800epoches.npy".format(model, subject))
+                subjectData = np.load("single_subjects\{}-cropped-singleSubjectNum{}-2.5sec-800epoches.npy".format(model, subject))
                 print(subjectData.shape)
                 if subject ==1 :        
                     if plotType == 'accuracy':
-                        plt.plot((1- subjectData[:,3]), color=colors['skyblue'], linestyle='dashed', linewidth=0.5, label='Single subjects train accuracy')
-                        plt.plot((1- subjectData[:,4]), color=colors['peachpuff'], linestyle='dashed', linewidth=0.5, label='Single subjects validation accuracy')
+                        plt.plot((1- subjectData[:,2]), color=colors['skyblue'], linestyle='dashed', linewidth=0.5, label='Single subjects train accuracy')
+                        plt.plot((1- subjectData[:,3]), color=colors['peachpuff'], linestyle='dashed', linewidth=0.5, label='Single subjects validation accuracy')
                         if withTest == True:
                             plt.plot((1- subjectData[:,5]), color=colors['lightgreen'], linestyle='dashed', linewidth=0.5, label='Single subjects test accuracy')
                     else:
-                        plt.plot(subjectData[:,3], color=colors['skyblue'], linestyle='dashed', linewidth=0.5, label='Single subjects train accuracy')
-                        plt.plot(subjectData[:,4], color=colors['peachpuff'], linestyle='dashed', linewidth=0.5, label='Single subjects validation accuracy')
+                        plt.plot(subjectData[:,2], color=colors['skyblue'], linestyle='dashed', linewidth=0.5, label='Single subjects train accuracy')
+                        plt.plot(subjectData[:,3], color=colors['peachpuff'], linestyle='dashed', linewidth=0.5, label='Single subjects validation accuracy')
                         if withTest == True:
                             plt.plot(subjectData[:,5], color=colors['lightgreen'], linestyle='dashed', linewidth=0.5, label='Single subjects test accuracy')
 
                 else:
                     if plotType == 'accuracy':
-                        plt.plot((1- subjectData[:,3]), color=colors['skyblue'], linestyle='dashed', linewidth=0.5)
-                        plt.plot((1- subjectData[:,4]), color=colors['peachpuff'], linestyle='dashed', linewidth=0.5)
+                        plt.plot((1- subjectData[:,2]), color=colors['skyblue'], linestyle='dashed', linewidth=0.5)
+                        plt.plot((1- subjectData[:,3]), color=colors['peachpuff'], linestyle='dashed', linewidth=0.5)
                         if withTest == True:
                             plt.plot((1- subjectData[:,5]), color=colors['lightgreen'], linestyle='dashed', linewidth=0.5)
 
                     else:
-                        plt.plot(subjectData[:,3], color=colors['skyblue'], linestyle='dashed', linewidth=0.5)
-                        plt.plot(subjectData[:,4], color=colors['peachpuff'], linestyle='dashed', linewidth=0.5)
+                        plt.plot(subjectData[:,2], color=colors['skyblue'], linestyle='dashed', linewidth=0.5)
+                        plt.plot(subjectData[:,3], color=colors['peachpuff'], linestyle='dashed', linewidth=0.5)
                         if withTest == True:
                             plt.plot(subjectData[:,5], color=colors['lightgreen'], linestyle='dashed', linewidth=0.5)
 
                 for index in range(0, len(subjectData)):
                     if plotType == 'accuracy':
-                        meanTrain[0, index] += 1- subjectData[index, 3]
-                        meanVal[0, index] += 1- subjectData[index, 4]
+                        meanTrain[0, index] += 1- subjectData[index, 2]
+                        meanVal[0, index] += 1- subjectData[index, 3]
                         if withTest == True:
                             meanTest[0, index] += 1- subjectData[index, 5]
                     else:
-                        meanTrain[0, index] += subjectData[index, 3]
-                        meanVal[0, index] += subjectData[index, 4]
+                        meanTrain[0, index] += subjectData[index, 2]
+                        meanVal[0, index] += subjectData[index, 3]
                         if withTest == True:
                             meanTest[0, index] += subjectData[index, 5]
                     meanTrain[1,index] += 1
@@ -82,9 +84,9 @@ for p in plotTypes:
         if withTest == True:
             plt.plot(meanTest[0]/meanTest[1], color=colors['darkgreen'], linestyle='solid', linewidth=1.5, label='Mean test accuracy')
         plt.legend(loc='best')
-        # plt.show()
+        plt.show()
         if withTest == True:
-            plt.savefig("mean-{}-plot-WITH_TEST-{}-trialwise-single_subjects-2.5sec-800epoches.png".format(plotType, model), bbox_inches='tight')
+            plt.savefig("mean-{}-plot-WITH_TEST-{}-cropped-finetune-2.5sec-800epoches.png".format(plotType, model), bbox_inches='tight')
         else:
-            plt.savefig("mean-{}-plot-{}-trialwise-single_subjects-2.5sec-800epoches.png".format(plotType, model), bbox_inches='tight')
+            plt.savefig("mean-{}-plot-{}-cropped-finetune-2.5sec-800epoches.png".format(plotType, model), bbox_inches='tight')
         plt.close()
--- proccesSingleScatterPlots.py
+++ proccesSingleScatterPlots.py
@@ -13,39 +13,50 @@ plotTypes = 'accuracy'
 
 colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
 
-accTestShallow = np.zeros(47)
-accTestDeep = np.zeros(47)
-j = 0
-for subject in range(1,53):
-    bestDeep = 0
-    bestShallow = 0
-    try:
-        subjectDataShallow = np.load("single_subjects\shallow-trialwise-singleSubjectNum{}-2.5sec-800epoches.npy".format(subject))
-        subjectDataDeep = np.load("single_subjects\deep-trialwise-singleSubjectNum{}-2.5sec-800epoches.npy".format(subject))
-        for i in range(0, len(subjectDataShallow)):
-            if subjectDataShallow[i, 4] >= bestShallow:
-                bestShallow = subjectDataShallow[i, 4]
-                accTestShallow[j] = subjectDataShallow[i, 5]
-        for i in range(0, len(subjectDataDeep)):
-            if subjectDataDeep[i, 4] >= bestDeep:
-                bestDeep = subjectDataDeep[i, 4]
-                accTestDeep[j] = subjectDataDeep[i, 5]
-        j += 1
-    except:
-        continue
+trainStrategies = ['cropped', 'trialwise']
 
-testMeanShallow = np.mean(accTestShallow)*100
-testMeanDeep = np.mean(accTestDeep)*100
-# plt.title('Accuracy rate / epoches')
-plt.ylabel('Accuracy [%]')
-plt.xlabel('Accuracy [%]')
-plt.plot(range(0, 101), color=colors['black'], linestyle='solid', linewidth=1)
-plt.scatter(accTestShallow*100, accTestDeep*100, marker='+', color=colors['red'], label='Single subjects accuracy', linestyle='solid', alpha=0.5, linewidth=1.5)
-plt.scatter(testMeanShallow, testMeanDeep, marker='o', color=colors['blue'], linestyle='solid', label='Mean accuracy', linewidth=1.5)
-plt.legend(loc='best')
-plt.show()
-# if withTest == True:
-#     plt.savefig("mean-{}-plot-WITH_TEST-{}-trialwise-single_subjects-2.5sec-800epoches.png".format(plotType, model), bbox_inches='tight')
-# else:
-#     plt.savefig("mean-{}-plot-{}-trialwise-single_subjects-2.5sec-800epoches.png".format(plotType, model), bbox_inches='tight')
-# plt.close()
+for shallowStrategy in trainStrategies:
+    for deepStrategy in trainStrategies:
+        accTestShallow = np.zeros(40)
+        accTestDeep = np.zeros(40)
+        j = 0
+        for subject in range(1,48):
+            if subject == 44 or subject == 45:
+                continue
+            bestDeep = 0
+            bestShallow = 0
+            try:
+                accTestShallow[j] = 1- np.load("finetuning\shallow-{}-singleSubjectNum{}-2.5sec-800epoches-testSetMisclass.npy".format(shallowStrategy, subject))
+
+                accTestDeep[j] = 1- np.load("finetuning\deep-{}-singleSubjectNum{}-2.5sec-800epoches-testSetMisclass.npy".format(deepStrategy, subject))
+                # for i in range(0, len(subjectDataShallow)):
+                #     if subjectDataShallow[i, 4] >= bestShallow:
+                #         bestShallow = subjectDataShallow[i, 4]
+                #         accTestShallow[j] = subjectDataShallow[i, 5]
+                # for i in range(0, len(subjectDataDeep)):
+                #     if subjectDataDeep[i, 4] >= bestDeep:
+                #         bestDeep = subjectDataDeep[i, 4]
+                #         accTestDeep[j] = subjectDataDeep[i, 5]
+                j += 1
+            except:
+                continue
+
+        testMeanShallow = np.mean(np.trim_zeros(accTestShallow))*100
+        testMeanDeep = np.mean(np.trim_zeros(accTestDeep))*100
+        # plt.title('Accuracy rate / epoches')
+        plt.ylabel('Accuracy [%]')
+        plt.xlabel('Accuracy [%]')
+
+        plt.plot(range(0, 101), color=colors['black'], linestyle='solid', linewidth=1)
+        plt.scatter(np.trim_zeros(accTestShallow)*100, np.trim_zeros(accTestDeep)*100, marker='+', color=colors['red'], label='Single subjects accuracy', linestyle='solid', alpha=0.5, linewidth=1.5)
+        plt.scatter(testMeanShallow, testMeanDeep, marker='o', color=colors['blue'], linestyle='solid', label='Mean accuracy', linewidth=1.5)
+        print("Training Strategies: Shallow = {}, Deep = {}".format(shallowStrategy, deepStrategy))
+        print("Mean accuracies: Shallow = {}, Deep = {}".format(testMeanShallow, testMeanDeep))
+        plt.legend(loc='best')
+        # plt.show()
+        # if withTest == True:
+        plt.savefig("scatterPlot-shallow_single_{}-deep_single_{}.png".format(shallowStrategy, deepStrategy), bbox_inches='tight')
+        plt.close()
+        # else:
+        #     plt.savefig("mean-{}-plot-{}-cropped-single_subjects-2.5sec-800epoches.png".format(plotType, model), bbox_inches='tight')
+        # plt.close()
