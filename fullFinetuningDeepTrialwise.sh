96f357c5a60452a3d0857d5fbb1b19e4832dbc6b
Your Name <you@example.com>
2019-06-10 20:11:06 +0000

    a

--- fullFinetuningDeepTrialwise.sh
+++ fullFinetuningDeepTrialwise.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for (( c = 26; c <= 52; c++ ))
+for (( c = 45; c <= 52; c++ ))
 do  
    python fintuneDeepTrialwise.py $c > finetuning\\deep-trialwise-subject$c-2.5sec-800epoches.txt
 done
