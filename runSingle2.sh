def549a6d58a6281b1b0b3a53b5224b59d8748ca
ron machine <you@example.com>
2019-10-05 08:44:54 +0000

    RonMachine - single-deep-cropped subjects 1-35

--- runSingle2.sh
+++ runSingle2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for (( c = 1; c <= 52; c++ ))
+for (( c = 14; c <= 52; c++ ))
 do  
    python singleSubjectTraining2.py $c > Logs\\SingleSubject\\deep-cropped\\deep-cropped-subject$c-400epoches.txt
 done
